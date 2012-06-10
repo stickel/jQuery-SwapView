399fd9042f974151c59ded19a3ad1a0f96a502cc
stickel <mike@screenflicker.com>
2012-06-10 01:02:28 -0700

    Initial working v0.1

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-jQuery-SwapView
-===============
+# jQuery SwapView
 
-jQuery plugin for swapping visibility of DOM elements
+jQuery plugin for swapping visibility of DOM elements.
+
+# Installation
+
+
+# Instructions
+
+1. Specify which DOM element you want to use as a trigger by giving it the class `.js-swapview-trigger`.
+2. Set which DOM elements will be the views changing with the classname `.js-swapview`
+3. Call the plugin on a DOM element containing both the trigger and swap views. For example, `$('#view-container').swapview();`
--- jquery.swapview.js
+++ jquery.swapview.js
@@ -1,17 +1,20 @@
 /*!
- * jquery lightweight plugin boilerplate
- * original author: @ajpiano
- * further changes, comments: @addyosmani
- * licensed under the mit license
+ * jQuery SwapView plugin
+ * Original author: @stickel
+ * Licensed under the MIT license
  */
 
 ;(function($, window, document, undefined) {
   var pluginName = 'SwapView',
       defaults = {
+        trigger: '.js-swapview-trigger',
+        views: '.js-swapview',
+        onBeforeSwap: function() {},
+        onAfterSwap: function() {}
       };
 
-  function SwapView(element, options) {
-    this.element = element;
+  function SwapView(container, options) {
+    this.container = container;
     this.options = $.extend({}, defaults, options);
     this._defaults = defaults;
     this._name = pluginName;
@@ -19,10 +22,50 @@
     this.init();
   }
 
-  Plugin.prototype = {
+  SwapView.prototype = {
     init: function() {
+      var container = this.container,
+          $container = $(container),
+          $trigger = $container.find(this.options.trigger),
+          views = this.options.views,
+          $views = $container.find(views),
+          $this = this;
+
+      // Set the default view visibility
+      $views.each(function() {
+        $(this).hide();
+        if ($(this).hasClass('active')) {
+          $(this).show();
+        }
+      });
+
+      // bind the trigger
+      $trigger.bind('click.swapview', function(e) {
+        e.preventDefault();
+        $this.options.onBeforeSwap.apply(this);
+        $this.swap($views);
+      });
     },
-    swap: function() {
+
+    swap: function(views) {
+      // default to the first view
+      var nextView = 0;
+
+      views.each(function(index, v) {
+        if ($(v).hasClass('active')) {
+          $(v).removeClass('active').hide();
+
+          if (index < views.length - 1) {
+            nextView = index + 1;
+          }
+        }
+      });
+
+      // Show the next view
+      $(views[nextView]).show().addClass('active');
+
+      // run the callback
+      this.options.onAfterSwap.apply(this);
     }
   };
 
@@ -30,8 +73,8 @@
     return this.each(function() {
       if (!$.data(this, 'plugin_' + pluginName)) {
         $.data(this, 'plugin_' + pluginName,
-               new Plugin(this, options));
+               new SwapView(this, options));
       }
     });
-  }
-})(jquery, window, document);
+  };
+})(jQuery, window, document);
